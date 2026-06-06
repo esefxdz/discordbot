e02020cd25ccc8281378dce6e92883c3fa121446
esefxdz <czesef31@proton.me>
2026-06-06 11:41:28 +0300

    added sysinfo.py

---
+++ cogs/sysinfo.py
@@ -0,0 +1,12 @@
+import subprocess
+import discord
+from discord.ext import commands
+
+class SysInfo(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command()
+    async def fetch(self, ctx):
+        result = subprocess.run(['fastfetch'], capture_output=True, text=True)
+        await ctx.reply(f'```\n{result.stdout}\n```')
