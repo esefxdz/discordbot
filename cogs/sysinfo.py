3679ba44af54df53138f3e0f82d3e37a2967d48e
esefxdz <czesef31@proton.me>
2026-06-06 12:00:55 +0300

    fix #3

--- cogs/sysinfo.py
+++ cogs/sysinfo.py
@@ -1,16 +1,20 @@
 import subprocess
 import discord
 from discord.ext import commands
+import re
 
 class SysInfo(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
+
+    def strip_ansi(self, text):
+        return re.sub(r'\x1b\[[0-9;]*[a-zA-Z]|\[[0-9]+[A-Z]', '', text)
 
     @commands.command()
     async def fetch(self, ctx):
         result = subprocess.run(['fastfetch', '--pipe'], capture_output=True, text=True)
-        await ctx.reply(f'```\n{result.stdout}\n```')
-
+        clean = self.strip_ansi(result.stdout)
+        await ctx.reply(f'```\n{clean}\n```')
 
 async def setup(bot):
     await bot.add_cog(SysInfo(bot))
