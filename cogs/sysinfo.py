26450de49a70841e1efbc111ffab4e7dbafd7eb8
esefxdz <czesef31@proton.me>
2026-06-06 11:51:31 +0300

    forgor.

--- cogs/sysinfo.py
+++ cogs/sysinfo.py
@@ -10,3 +10,6 @@ class SysInfo(commands.Cog):
     async def fetch(self, ctx):
         result = subprocess.run(['fastfetch'], capture_output=True, text=True)
         await ctx.reply(f'```\n{result.stdout}\n```')
+
+    async def setup(bot):
+    await bot.add_cog(SysInfo(bot))
