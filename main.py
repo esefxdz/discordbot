2ecd6bc4cb20d5904cd7b427b6bacfcbdc4e0a11
esefxdz <czesef31@proton.me>
2026-06-06 11:43:38 +0300

    updated main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ async def main():
         await bot.load_extension('cogs.chance')
         await bot.load_extension('cogs.gifs')
         await bot.load_extension('cogs.copypasta')
+        await bot.load_extension('cogs.sysinfo')
         await bot.start(os.getenv('DISCORD_TOKEN'))
 
 logging.basicConfig(level=logging.INFO)
