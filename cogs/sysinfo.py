b9ac1f32108cd451cffed1d0f1ab4029652fd3a7
esefxdz <czesef31@proton.me>
2026-06-06 11:53:55 +0300

    accidentally intended inside class while being function

--- cogs/sysinfo.py
+++ cogs/sysinfo.py
@@ -11,5 +11,6 @@ class SysInfo(commands.Cog):
         result = subprocess.run(['fastfetch'], capture_output=True, text=True)
         await ctx.reply(f'```\n{result.stdout}\n```')
 
-    async def setup(bot):
-        await bot.add_cog(SysInfo(bot))
+
+async def setup(bot):
+    await bot.add_cog(SysInfo(bot))
